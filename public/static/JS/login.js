9b23c7c13e5cd0e75b84b163076eccb515769b04
Kurkin Nickolay <mrnickrus@gmail.com>
2017-09-06 20:20:31 +0300

    # This is a combination of 4 commits. # This is the 1st commit message:
    login and menu alpha version
    
    # This is the commit message #2:
    
    some fixes
    
    # This is the commit message #3:
    
    spa
    
    # This is the commit message #4:
    
    data validation

--- public/static/JS/login.js
+++ public/static/JS/login.js
@@ -1,30 +1,40 @@
-const loginForm = document.getElementById("login-form")
-const registerForm = document.getElementById('register-form');
-const loginMsg = document.getElementById('login-message');
-const signupMsg = document.getElementById('reg-message');
-const createBtn = document.getElementById('create-btn');
+const application = document.getElementById('application');
+const nav = document.getElementById('navigation');
+const warningMsgLog = document.getElementById('warning-msg-log');
+const warningMsgReg = document.getElementById('warning-msg-reg');
+const liveSectionCollection = application.getElementsByTagName('section');
 const loginBtn = document.getElementById('login-btn');
+const registerBtn = document.getElementById('create-btn');
+
+const sections = [
+    ['login-sec', 'SIGN IN'],
+    ['register-sec', 'SIGN UP'],
+    ['menu-sec', 'MENU'],
+    ['about-sec', 'ABOUT'],
+];
 
 
-function changeForm(event) {
-    event.preventDefault();
-    loginForm.classList.toggle('hidden');
-    registerForm.classList.toggle('hidden');
+for (let sect of sections) {
+    const button = document.createElement('input');
+    button.setAttribute('type', 'button');
+    button.setAttribute('class', 'button-nav');
+
+    button.setAttribute('data-section', sect[0]);
+    button.value = sect[1];
+    nav.appendChild(button);
 }
 
-function changeP(id, flag) {
-    if (flag) {
-        id.classList.remove('hidden');
-    } else {
-        id.classList.add('hidden');
-    }
-}
 
-loginMsg.addEventListener('click', changeForm);
-signupMsg.addEventListener('click', changeForm);
+nav.addEventListener('click', (event) => {
 
-createBtn.addEventListener('click', (event) => {
-    event.preventDefault();
+    const sectionID = event.target.getAttribute('data-section');
+    const sectionsArray = Array.from(liveSectionCollection);
+    sectionsArray.forEach((sectionElement) => {
+        sectionElement.hidden = true;
+        if (sectionElement.id === sectionID) {
+            sectionElement.hidden = false;
+        }
+    });
 }, false);
 
 loginBtn.addEventListener('click', (event) => {
@@ -32,16 +42,40 @@ loginBtn.addEventListener('click', (event) => {
     let nick = document.getElementById('nick-log').value;
     let pas = document.getElementById('password-log').value;
 
-    changeP(document.getElementById('not-correct-data-p'), (nick.length > 15 || pas.length > 20));
-
-    if (false) { //some server logic for login
-        debugger;
+    if (nick.length > 15 || pas.length > 20 || nick.length < 1 || pas.length < 1) {
+        warningMsgLog.innerHTML = "Invalid Data";
+        warningMsgLog.hidden = false;
     } else {
-        changeP(document.getElementById('not-correct-p'), true);
+        if (true) { //some server logic for login
+            //debugger;
+        } else {
+            warning_msg.hidden = true;
+            innerHTML = "Wrong Nick or Password";
+            warningMsgLog.hidden = false;
+        }
     }
 }, false);
 
+registerBtn.addEventListener('click', (event) => {
+    event.preventDefault();
+    const nick = document.getElementById('nick-reg').value;
+    const pas = document.getElementById('password-reg').value;
+    const conf = document.getElementById('confirm-reg').value;
 
-
-// интерфейсы - все дз(размещаются на портале) проверяются на рк(3шт)
-// фронт - дз(размещаются на https://frontend-park-mailru.firebaseapp.com/) заливать за сутки до рк
+    if (nick.length > 15 || pas.length > 20 || conf.length > 20 || nick.length < 1 || pas.length < 1 || conf.length < 1) {
+        warningMsgReg.innerHTML = "Invalid Data";
+        warningMsgReg.hidden = false;
+    } else {
+        if (conf !== pas) {
+            warningMsgReg.innerHTML = "Passwords do not match";
+            warningMsgReg.hidden = false;
+        } else {
+            if (true) { //some server logic for login
+                //debugger;
+            } else {
+                warningMsgReg.innerHTML = "This Nick already exists";
+                warningMsgReg.hidden = false;
+            }
+        }
+    }
+}, false);
