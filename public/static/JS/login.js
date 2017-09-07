199633b2676c1eed3e3d1a8a6e5cd934bf10cd2e
Crusader727 <rubenhov97@gmail.com>
2017-09-07 14:55:48 +0300

    some fixes

--- public/static/JS/login.js
+++ public/static/JS/login.js
@@ -1,27 +1,21 @@
 const loginForm = document.getElementById("login-form")
 const registerForm = document.getElementById('register-form');
 const loginMsg = document.getElementById('login-message');
-const signupMsg = document.getElementById('reg-message');
+const registerMsg = document.getElementById('reg-message');
 const createBtn = document.getElementById('create-btn');
 const loginBtn = document.getElementById('login-btn');
-
+const registerBtn = document.getElementById('create-btn');
+const warning_msg = document.getElementById('warning-msg');
 
 function changeForm(event) {
     event.preventDefault();
+    warning_msg.classList.add('hidden');
     loginForm.classList.toggle('hidden');
     registerForm.classList.toggle('hidden');
 }
 
-function changeP(id, flag) {
-    if (flag) {
-        id.classList.remove('hidden');
-    } else {
-        id.classList.add('hidden');
-    }
-}
-
 loginMsg.addEventListener('click', changeForm);
-signupMsg.addEventListener('click', changeForm);
+registerMsg.addEventListener('click', changeForm);
 
 createBtn.addEventListener('click', (event) => {
     event.preventDefault();
@@ -32,12 +26,40 @@ loginBtn.addEventListener('click', (event) => {
     let nick = document.getElementById('nick-log').value;
     let pas = document.getElementById('password-log').value;
 
-    changeP(document.getElementById('not-correct-data-p'), (nick.length > 15 || pas.length > 20));
-
-    if (false) { //some server logic for login
-        debugger;
+    if (nick.length > 15 || pas.length > 20 || nick.length < 1 || pas.length < 1) {
+        warning_msg.innerHTML = "Invalid Data";
+        warning_msg.classList.remove('hidden');
     } else {
-        changeP(document.getElementById('not-correct-p'), true);
+        if (true) { //some server logic for login
+            //debugger;
+        } else {
+            warning_msg.innerHTML = "Wrong Nick or Password";
+            warning_msg.classList.remove('hidden');
+        }
+    }
+}, false);
+
+registerBtn.addEventListener('click', (event) => {
+    event.preventDefault();
+    let nick = document.getElementById('nick-reg').value;
+    let pas = document.getElementById('password-reg').value;
+    let conf = document.getElementById('confirm-reg').value;
+
+    if (nick.length > 15 || pas.length > 20 || conf.length > 20 || nick.length < 1 || pas.length < 1 || conf.length < 1) {
+        warning_msg.innerHTML = "Invalid Data";
+        warning_msg.classList.remove('hidden');
+    } else {
+        if (conf !== pas) {
+            warning_msg.innerHTML = "Passwords do not match";
+            warning_msg.classList.remove('hidden');
+        } else {
+            if (true) { //some server logic for login
+                //debugger;
+            } else {
+                warning_msg.innerHTML = "This Nick already exists";
+                warning_msg.classList.remove('hidden');
+            }
+        }
     }
 }, false);
 
@@ -45,3 +67,5 @@ loginBtn.addEventListener('click', (event) => {
 
 // интерфейсы - все дз(размещаются на портале) проверяются на рк(3шт)
 // фронт - дз(размещаются на https://frontend-park-mailru.firebaseapp.com/) заливать за сутки до рк
+// swager
+//<p class="hidden warning" id="not-correct-data-p">Invalid Data</p>
