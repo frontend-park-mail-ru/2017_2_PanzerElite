d3f0c7bb9ad490d16f056eb680c9e7048edac833
Crusader727 <rubenhov97@gmail.com>
2017-09-29 17:53:48 +0300

    awd

---
+++ old-public/static/js/config.js
@@ -0,0 +1,10 @@
+const PREFIX_URL = "https://salty-shelf-19870.herokuapp.com/api/user/";
+const urls = {
+    login: PREFIX_URL + "login",
+    register: PREFIX_URL + "register",
+    logout: PREFIX_URL + "logout",
+    check: PREFIX_URL + "getuser",
+    chagePassword: PREFIX_URL + "changepassword"
+
+};
+export default urls;
