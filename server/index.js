221b52e6268bde3661ea3b2407cd6c2fbed1b0f5
Crusader727 <rubenhov97@gmail.com>
2017-09-29 17:29:03 +0300

    for heroku

--- server/index.js
+++ server/index.js
@@ -1,9 +1,19 @@
 const express = require('express');
+const body = require('body-parser');
+const cookie = require('cookie-parser');
+const morgan = require('morgan');
+const uuid = require('uuid/v4');
 const app = express();
 
+
+app.use(morgan('dev'));
+app.use(express.static('public'));
+app.use('/node_modules', express.static(__dirname + '/../node_modules'));
+app.use(body.json());
+app.use(cookie());
 app.use(express.static('public'));
 
-app.get('../public/bandles/*', (req, res) => {
+app.get('../public/*', (req, res) => {
     res.send('404');
 })
 
