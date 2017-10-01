aec28f32a6578dfa004132c3d8b10f8dd4d812e8
Kurkin Nickolay <mrnickrus@gmail.com>
2017-10-01 22:19:30 +0300

    fixed change

--- public/main.js
+++ public/main.js
@@ -1,7 +1,7 @@
 import { MainGenerator } from "./modules/mainGenerator.js";
 import { Block } from "./block/block.js";
 import UserService from "./services/user-service.js";
-import PBar from "./modules/load-bar.js";
+//import PBar from "./modules/load-bar.js";
 
 const app = document.getElementById("application");
 const main = new Block("div", {});
@@ -10,52 +10,52 @@ app.appendChild(main.el);
 MainGenerator(main);
 
 const userService = new UserService(main);
-const pBar = new PBar();
+//const pBar = new PBar();
 
-pBar.show();
+//pBar.show();
 userService.whoami();
-pBar.hide();
+//pBar.hide();
 
 main.login.loginBtn.setCallback(() => {
-	pBar.show();
-	userService.login(main.login.nick.el.value,
-		main.login.password.el.value);
-	pBar.hide();
+    //pBar.show();
+    userService.login(main.login.nick.el.value,
+        main.login.password.el.value);
+    //pBar.hide();
 });
 main.login.changeformBtn.setCallback(() => {
-	main.login.hide();
-	main.register.show();
+    main.login.hide();
+    main.register.show();
 });
 
 main.register.registerBtn.setCallback(() => {
-	pBar.show();
-	userService.register(main.register.nick.el.value,
-		main.register.password.el.value, main.register.confirm.el.value);
-	pBar.hide();
+    //pBar.show();
+    userService.register(main.register.nick.el.value,
+        main.register.password.el.value, main.register.confirm.el.value);
+    //pBar.hide();
 
 });
 main.register.changeformBtn.setCallback(() => {
-	main.login.show();
-	main.register.hide();
+    main.login.show();
+    main.register.hide();
 });
 
 main.menu.changeBtn.setCallback(() => {
-	main.menu.hide();
-	main.change.show();
+    main.menu.hide();
+    main.change.show();
 });
 main.menu.logoutBtn.setCallback(() => {
-	pBar.show();
-	userService.logout();
-	pBar.hide();
+    //pBar.show();
+    userService.logout();
+    //pBar.hide();
 });
 
 main.change.changeBtn.setCallback(() => {
-	pBar.show();
-	userService.changePassword(main.change.password.el.value,
-		main.change.confirm.el.value);
-	pBar.hide();
+    //pBar.show();
+    userService.changePassword(main.change.password.el.value,
+        main.change.confirm.el.value);
+    //pBar.hide();
 });
 main.change.changeformBtn.setCallback(() => {
-	main.chagne.hide();
-	main.menu.show();
+    main.change.hide();
+    main.menu.show();
 });
--- public/services/user-service.js
+++ public/services/user-service.js
@@ -1,105 +1,110 @@
 import urls, { GET, POST } from "../configs/config.js";
 import httpReq from "../modules/http.js";
 import Validate from "../modules/validation.js";
+import PBar from "../modules/load-bar.js";
 
+const pBar = new PBar();
 
 export default class UserService {
-	constructor(main) { this.main = main; }
+    constructor(main) { this.main = main; }
 
 
-	login(nick, pas) {
-		if (!Validate.checkLogAndPas(nick, pas)) {
-			//sections.login.loginform.warningMsg("Invalid Data", false);
-			//his.main.login.warning
-		} else {
+    login(nick, pas) {
+        if (!Validate.checkLogAndPas(nick, pas)) {
+            //sections.login.loginform.warningMsg("Invalid Data", false);
+            //his.main.login.warning
+        } else {
 
-			httpReq(POST, urls.login, {
-				login: nick,
-				password: pas
-			})
-				.then(() => {
-					this.whoami();
-				})
-				.catch(err => {
-					//sections.login.loginform.warningMsg("Wrong Nick or Password", false);
-					console.log(err);
+            httpReq(POST, urls.login, {
+                    login: nick,
+                    password: pas
+                })
+                .then(() => {
+                    this.whoami();
+                })
+                .catch(err => {
+                    //sections.login.loginform.warningMsg("Wrong Nick or Password", false);
+                    console.log(err);
 
-				});
-		}
-	}
+                });
+        }
+    }
 
-	register(nick, pas, conf) {
-		if (!Validate.checkLogAndPas(nick, pas)) {
-			console.log("invalid data");
-			//sections.register.registerform.warningMsg("Invalid Data", false);
-		} else {
-			if (!Validate.confirmPassword(conf, pas)) {
-				console.log("ps dont match");
-				//sections.register.registerform.warningMsg("Passwords Dont Match", false);
-			} else {
-				console.log("hey im in register sevice");
-				httpReq(POST, urls.register, { login: nick, password: pas, cf: conf })
-					.then(() => {
-						this.whoami();
-					})
-					.catch(err => {
-						//sections.register.registerform.warningMsg("This Nick already Exists", false);
-						console.log(err);
+    register(nick, pas, conf) {
+        if (!Validate.checkLogAndPas(nick, pas)) {
+            console.log("invalid data");
+            //sections.register.registerform.warningMsg("Invalid Data", false);
+        } else {
+            if (!Validate.confirmPassword(conf, pas)) {
+                console.log("ps dont match");
+                //sections.register.registerform.warningMsg("Passwords Dont Match", false);
+            } else {
+                console.log("hey im in register sevice");
+                httpReq(POST, urls.register, { login: nick, password: pas, cf: conf })
+                    .then(() => {
+                        this.whoami();
+                    })
+                    .catch(err => {
+                        //sections.register.registerform.warningMsg("This Nick already Exists", false);
+                        console.log(err);
 
-					});
-			}
-		}
-	}
-
-
-	logout() {
-		//sections.login.loginform.warningMsg("", true);
-		//sections.register.registerform.warningMsg("", true);
-
-		httpReq(GET, urls.logout)
-			.then(() => {
-				this.whoami();
-			})
-			.catch(err => {
-				console.log(err);
-
-			});
-	}
+                    });
+            }
+        }
+    }
 
 
-	changePassword(pas, conf) {
-		if (!Validate.checkPassword(pas)) {
-			//sections.change.changeform.warningMsg("Invalid Data", false);
-		} else {
-			if (!Validate.confirmPassword(conf, pas)) {
-				//sections.change.changeform.warningMsg("Passwords Dont Match", false);
-			} else {
+    logout() {
+        //sections.login.loginform.warningMsg("", true);
+        //sections.register.registerform.warningMsg("", true);
 
-				httpReq(POST, urls.chagePassword, { password: pas, conf })
-					.then(() => {
-						this.whoami();
-					})
-					.catch(err => {
-						console.log(err);
+        httpReq(GET, urls.logout)
+            .then(() => {
+                this.whoami();
+            })
+            .catch(err => {
+                console.log(err);
 
-					});
-			}
-		}
-	}
+            });
+    }
 
 
-	whoami() {
-		this.main.login.hide();
-		this.main.register.hide();
-		this.main.menu.hide();
-		this.main.change.hide();
-		httpReq(GET, urls.check)
-			.then(() => {
-				this.main.menu.show();
-			})
-			.catch(error => {
-				this.main.login.show();
-				console.log(error);
-			});
-	}
+    changePassword(pas, conf) {
+        if (!Validate.checkPassword(pas)) {
+            //sections.change.changeform.warningMsg("Invalid Data", false);
+        } else {
+            if (!Validate.confirmPassword(conf, pas)) {
+                //sections.change.changeform.warningMsg("Passwords Dont Match", false);
+            } else {
+
+                httpReq(POST, urls.chagePassword, { password: pas, conf })
+                    .then(() => {
+                        this.whoami();
+                    })
+                    .catch(err => {
+                        console.log(err);
+
+                    });
+            }
+        }
+    }
+
+
+    whoami() {
+        pBar.show();
+        this.main.login.hide();
+        this.main.register.hide();
+        this.main.menu.hide();
+        this.main.change.hide();
+        httpReq(GET, urls.check)
+            .then(() => {
+                this.main.menu.show();
+                pBar.hide();
+            })
+            .catch(error => {
+                this.main.login.show();
+                pBar.hide();
+                console.log(error);
+            });
+    }
 }
