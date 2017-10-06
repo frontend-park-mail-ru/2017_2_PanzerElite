ddd4a3f9d8b2a52129a2788944120b2c3f1084b5
Crusader727 <rubenhov97@gmail.com>
2017-10-06 17:48:38 +0300

    removed trash from rep :)

--- pugCreator.js
+++
@@ -1,9 +0,0 @@
-const fs = require("fs");
-const pug = require("pug");
-const path = require("path");
-const DATA = path.resolve("./");
-
-
-const scorePWD = DATA + "/public/blocks/scoreboard/";
-let scoreFunc = pug.compileFileClient(scorePWD + "scoreboard.pug", { name: "scoreboardTEMPLATE" });
-fs.writeFileSync(scorePWD + "ScoreboardTEMPLATE.js", scoreFunc);
