b0d0c602da5d8475c96639ae73917564a89e127f
Crusader727 <rubenhov97@gmail.com>
2017-10-06 17:03:57 +0300

    for heroku

---
+++ previous-public/configs/menu-fields.js
@@ -0,0 +1,25 @@
+"use strict";
+
+const menuFields = [{
+	attrs: {
+		class: "button",
+		type: "button",
+		value: "Change Password",
+	}
+},
+{
+	attrs: {
+		class: "button",
+		type: "button",
+		value: "log out",
+	}
+},
+{
+	attrs: {
+		class: "button",
+		type: "button",
+		value: "scoreboard",
+	}
+}
+];
+export default menuFields;
---
+++ previous-public/modules/http.js
@@ -0,0 +1,25 @@
+/**
+ * Модуль, предоставляющий методы для выполнения HTTP-запросов
+ * @module Http
+ */
+function httpReq(type, uRL, sendObject) {
+	return new Promise(function(resolve, reject) {
+		fetch(uRL, {
+			method: type,
+			mode: "cors",
+			credentials: "include",
+			body: JSON.stringify(sendObject),
+			headers: {
+				"Content-Type": "application/json; charset=utf-8"
+			}
+		}).then(function(response) {
+			if (response.status === 200) {
+				resolve();
+			} else {
+				reject("Something went wrong");
+			}
+		});
+	});
+
+}
+export default httpReq;
