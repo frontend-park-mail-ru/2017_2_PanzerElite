2ecbdf143c71d2970f9d59a53cd7afa2e2952c73
112Nick <mrnickrus@gmail.com>
2017-12-07 15:46:58 +0300

    heroku deploy

--- public/configs/config.js
+++ public/configs/config.js
@@ -1,5 +1,5 @@
-// const PREFIX_URL = "https://salty-shelf-19870.herokuapp.com/api/user/";
-const PREFIX_URL = "http://127.0.0.1:8080/api/user/";
+const PREFIX_URL = "https://salty-shelf-19870.herokuapp.com/api/user/";
+// const PREFIX_URL = "http://127.0.0.1:8080/api/user/";
 const urls = {
     login: PREFIX_URL + "login",
     register: PREFIX_URL + "register",
